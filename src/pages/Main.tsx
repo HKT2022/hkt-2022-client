6c2ef805c00ff63b40976abf1c491d9a19f0f295
noname <48761044+noname0310@users.noreply.github.com>
2022-07-14 18:23:44 +0900

    ListViewRenderer

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -15,9 +15,12 @@ import {
     PaddingDiv
 } from '../components/atoms/styled';
 import NavBar from '../components/organisms/NavBar';
+import PagedListView from '../components/organisms/PagedListView';
 
 const MainFlexDiv = styled(OuterFlexDiv)`
     justify-content: flex-start;
+    min-height: 100%;
+    height: auto;
 `;
 
 const Container = styled(InnerFlexDiv1)`
@@ -45,6 +48,12 @@ function Main(): JSX.Element {
             <Container>
                 <PaddedButton onClick={handlePasswordChangeClick}>Change Password</PaddedButton>
             </Container>
+            <PagedListView items={[
+                {id: '1', name: 'test'},
+                {id: '2', name: 'test2'},
+                {id: '3', name: 'test3'},
+                {id: '4', name: 'test4'}
+            ]} pageCount={10} currentPage={3} onPageChange={() => {/* */}}/>
         </MainFlexDiv>
     );
 }
