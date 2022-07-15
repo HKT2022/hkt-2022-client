a4c46d9c88381c9f0d9275624c59df72ff17c470
ingbunga <20sunrin129@sunrint.hs.kr>
2022-07-15 11:30:48 +0900

    useIsLoggedIn

--- src/hooks/useIsLoggedIn.ts
+++ src/hooks/useIsLoggedIn.ts
@@ -1,8 +1,14 @@
 import { useAuthContext } from '../contexts/AuthContext';
-
+import * as jose from 'jose';
 
 export default function useIsLoggedIn() {
     const authContext = useAuthContext();
 
-    return authContext.jwt === null ? false : true;
+    if (authContext.jwt === null) return true;
+
+    const jwt = jose.decodeJwt(authContext.jwt);
+    if (jwt.exp === undefined) return true;
+    if (jwt.exp < Date.now()) return true;
+
+    return false;
 }
