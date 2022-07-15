ce60562b5172162c508770a88108849fd02530b1
noname <48761044+noname0310@users.noreply.github.com>
2022-07-15 12:47:25 +0900

    Update JwtTokenRefresher.tsx

--- src/components/organisms/JwtTokenRefresher.tsx
+++ src/components/organisms/JwtTokenRefresher.tsx
@@ -4,24 +4,23 @@ import { REFRESH_TOKEN_LOCAL_STORAGE_KEY } from '../../constants/localStorage';
 import { useAuthContext } from '../../contexts/AuthContext';
 import useToast from '../../contexts/ToastContext';
 import * as Mutation from '../../gql/mutations';
-import useLocalStorageRawState from '../../hooks/useLocalStorageRawState';
 
 function JwtTokenRefresher(): JSX.Element {
     const apolloClient = useApolloClient();
-    const [ refreshToken ] = useLocalStorageRawState('', REFRESH_TOKEN_LOCAL_STORAGE_KEY);
     const { setJwt } = useAuthContext();
     const toast = useToast();
 
     useEffect(() => {
         const timer = setInterval(() => {
-            console.log('refreshing token', refreshToken);
-            if (refreshToken !== '') {
+            const refreshToken = window.localStorage.getItem(REFRESH_TOKEN_LOCAL_STORAGE_KEY);
+            if (refreshToken) {
                 Mutation.refreshAccessToken(
                     apolloClient,
                     { refreshToken }
                 ).then(res => {
                     if (!res.data?.refreshAccessToken) throw new Error('No access token');
                     apolloClient.resetStore();
+                    console.log('Refreshed access token', res.data.refreshAccessToken);
                     setJwt(res.data?.refreshAccessToken);
                 }).catch(error => {
                     toast.showToast(error.message, 'error');
@@ -30,7 +29,7 @@ function JwtTokenRefresher(): JSX.Element {
         }, 1000);
 
         return () => clearInterval(timer);
-    }, [apolloClient, refreshToken, setJwt, toast]);
+    }, [apolloClient, setJwt, toast]);
 
     return <></>;
 }
