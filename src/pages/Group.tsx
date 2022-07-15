b060f73ba78d8d2b56815adaa9e4f22b44e65b10
noname <48761044+noname0310@users.noreply.github.com>
2022-07-16 03:58:00 +0900

    Update Group.tsx

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -4,65 +4,70 @@ import { OuterFlexDiv } from '../components/atoms/styled';
 import { MEDIA_MAX_WIDTH } from '../constants/css';
 
 export const ContainerDiv = styled.div`
-width: calc(max(100% - 200px, 280px));
-position: relative;
+    width: calc(max(100% - 200px, 280px));
+    position: relative;
 
-@media (max-width: ${MEDIA_MAX_WIDTH + 60}px) {
-    width: calc(100% - 30px);
-}
+    @media (max-width: ${MEDIA_MAX_WIDTH + 60}px) {
+        width: calc(100% - 30px);
+    }
 `;
 export const TitleContainerDiv = styled.div`   
-height: 185px;
-position: relative;
-z-index: 2;
+    height: 185px;
+    position: relative;
+    z-index: 2;
 `;
+
 export const TitleH1 = styled.h1`
-text-align: center;
-color: white;
-margin-bottom: 0px;
-margin-top: 0px;
-padding-top: 70px;
-font-size: 45px;
+    text-align: center;
+    color: white;
+    margin-bottom: 0px;
+    margin-top: 0px;
+    padding-top: 70px;
+    font-size: 45px;
 `;
+
 const BodyContainerDiv = styled.div`
-height: calc(100vh - 185px);
+    height: calc(100vh - 185px);
 `;
+
 const ListContainerDiv = styled.div`
-width: 100%;
-border-radius: 30px;
-background-color: white;
-box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.12);
-padding: 38px;
-box-sizing: border-box;
-height: calc(100% - 52px);
+    width: 100%;
+    border-radius: 30px;
+    background-color: white;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.12);
+    padding: 38px;
+    box-sizing: border-box;
+    height: calc(100% - 52px);
 
-@media (max-width: ${MEDIA_MAX_WIDTH + 60}px) {
-    padding: 20px;
-}
+    @media (max-width: ${MEDIA_MAX_WIDTH + 60}px) {
+        padding: 20px;
+    }
 `;
+
 const ListOl = styled.ol`
-width: 100%;
-list-style: none;
-padding: 0px;
-margin: 0px;
+    width: 100%;
+    list-style: none;
+    padding: 0px;
+    margin: 0px;
 `;
-const ListItemLi = styled.li<{ isReversedColor: boolean }>`
-width: 100%;
-border-radius: 21.5px;
-border-style: solid;
-border-width: 1px;
-border-color: ${props => props.theme.colors.primary};
-background-color: ${props => props.isReversedColor ? props.theme.colors.primary : 'white'};
-height: 43px;
-margin-bottom: 16px;
-display: flex;
-padding: 9px 12px;
-box-sizing: border-box;
-display: inline-flex;
-justify-content: space-between;
-color: ${props => props.isReversedColor ? 'white' : props.theme.colors.primary};
 
-font-size: 19px;
+const ListItemLi = styled.li<{ isReversedColor: boolean }>`
+    width: 100%;
+    border-radius: 21.5px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: ${props => props.theme.colors.primary};
+    background-color: ${props => props.isReversedColor ? props.theme.colors.primary : 'white'};
+    height: 43px;
+    margin-bottom: 16px;
+    display: flex;
+    padding: 9px 12px;
+    box-sizing: border-box;
+    display: inline-flex;
+    justify-content: space-between;
+    color: ${props => props.isReversedColor ? 'white' : props.theme.colors.primary};
+
+    font-size: 19px;
 `;
 
 // const VerticalLineDiv = styled.div<{ isReversedColor: boolean }>`
@@ -75,9 +80,9 @@ font-size: 19px;
 // `;
 
 const VerticalCenterDiv = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 // const RankSpan = styled.span`
@@ -91,12 +96,13 @@ const UserNameSpan = styled.span`
 // `;
 
 export const LightBlueBallContainer = styled.div`
-display: flex;
-height: 0px;
-position: relative;
-justify-content: center;
-z-index: 1;
+    display: flex;
+    height: 0px;
+    position: relative;
+    justify-content: center;
+    z-index: 1;
 `;
+
 export const LightBlueBall = styled.div`
     background-color: #CEDEFF;
     width: 452px;
@@ -106,16 +112,16 @@ export const LightBlueBall = styled.div`
 `;
 
 const BackLink = styled(Link)`
-z-index: 3;
-display: inline-block;
-position: absolute;
-left: 30px;
+    z-index: 3;
+    display: inline-block;
+    position: absolute;
+    left: 30px;
 top: 30px;
 `;
 const BackIcon = styled.img`
-width: 30px;
-height: 30px;
-cursor: pointer;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
 `;
 
 function RankingList({ rankings }: { rankings: string[] }) {
