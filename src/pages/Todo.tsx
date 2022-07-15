8b55f3925f9522f5f710c5e8f7e725bccb48b159
noname <48761044+noname0310@users.noreply.github.com>
2022-07-15 15:33:42 +0900

    Update Todo.tsx

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,61 +1,168 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import HealthBar from '../components/atoms/HealthBar';
-import CenterAlignedPage from '../components/templates/CenterAlignedPage';
-import useUser from '../hooks/useCurrentUser';
+import { OuterFlexDiv, PaddingDiv } from '../components/atoms/styled';
 
-
-const Base = styled.div`
+const BaseDiv = styled.div`
     display: flex;
     flex-direction: row;
+    color: ${props => props.theme.colors.primaryInverse};
 `;
 
-const LeftSide = styled.div`
+const LeftSideDiv = styled.div`
 `;
 
-const RightSide = styled.div`
-    margin: 10px;
+const RightSideDiv = styled.div`
+    border: 1px solid red;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `;
 
+const TitleContainerDiv = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 20px 0px;
+`;
+
+const HealthBarContainerDiv = styled.div`
+    border: 1px solid red;
+    padding: 10px;
+    margin-top: 10px;
+`;
+
+const TodoItemDiv = styled.div`
+    border: 1px solid red;
+    border-radius: 50px;
+
+    width: 400px;
+    margin: 8px;
+    padding: 8px 12px;
+
+    font-size: 13px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const TodoItemLeftDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+interface PriorityDivProps {
+    priority: 1 | 2 | 3;
+}
+
+const PriorityDiv = styled.div<PriorityDivProps>`
+    border: 1px solid red;
+    border-radius: 50px;
+    width: 10px;
+    height: 10px;
+    margin-right: 8px;
+
+    background-color: ${props => {
+        switch (props.priority) {
+        case 1:
+            return '#ff0000';
+        case 2:
+            return '#ffa500';
+        case 3:
+            return '#00ff00';
+        default:
+            return '#ffffff';
+        }
+    }};
+`;
+
+const TodoItemButton = styled.button`
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
+
+    background-color: yellow;
+    border: 1px solid ${props => props.theme.colors.primary};
+`;
+
+const TodoListAddForm = styled.form`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+`;
+
+const TodoListInput = styled.input`
+    border-radius: 50px;
+    margin: 8px;
+    padding: 5px 10px;
+    box-sizing: content-box;
+    width: 100%;
+`;
+
+const TodoListAddButton = styled.button`
+    border-radius: 50%;
+    width: 25px;
+    height: 25px;
+    box-sizing: content-box;
+
+    margin-right: 10px;
+`;
 
 function Todo(): JSX.Element {
-    const user = useUser();
-
     const [todos, ] = useState<string[]>([
         'Todo 1',
         'Todo 2',
+        'Todo 3'
     ]);
 
     return (
-        <CenterAlignedPage>
-            <Base>
-                <LeftSide>
-                    <div>
-                        <h1 style={{ color: 'white' }}>{user?.username || '샌즈'}</h1>
-                    </div>
-                    <div style={{ color: 'white' }}>
-                        캐릭터 체력
-                        <HealthBar health={10} maxHealth={100} />
-                    </div>
+        <OuterFlexDiv>
+            <BaseDiv>
+                <LeftSideDiv>
                     <div style={{width: 300, height: 300, background: 'white'}}>
-                        캐릭터 사진
+                        charactor
                     </div>
-                </LeftSide>
-                <RightSide>
-                    <input placeholder='새로운 todo 추가' />
+                    <HealthBarContainerDiv>
+                        health
+                        <HealthBar health={10} maxHealth={100} />
+                    </HealthBarContainerDiv>
+                </LeftSideDiv>
+                <PaddingDiv width='10px' />
+                <RightSideDiv>
+                    <TitleContainerDiv>
+                        To Do
+                    </TitleContainerDiv>
                     <div>
                         {todos.map(todo => {
                             return (
-                                <div key={todo} style={{color: 'white'}}>
-                                    {todo}
-                                    <button>V</button>
-                                </div>
+                                <TodoItemDiv key={todo}>
+                                    <TodoItemLeftDiv>
+                                        <PriorityDiv priority={1} />
+                                        {todo}
+                                    </TodoItemLeftDiv>
+                                    <TodoItemButton>
+                                        v
+                                    </TodoItemButton>
+                                </TodoItemDiv>
                             );
                         })}
                     </div>
-                </RightSide>
-            </Base>
-        </CenterAlignedPage>
+                    <TodoListAddForm>
+                        <TodoListInput placeholder='new todo...' />
+                        <TodoListAddButton>
+                            +
+                        </TodoListAddButton>
+                    </TodoListAddForm>
+                </RightSideDiv>
+            </BaseDiv>
+        </OuterFlexDiv>
     );
 }
 
